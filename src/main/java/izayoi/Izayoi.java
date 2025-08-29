5409f82d38d586feeda7a2d0830f351d98251f9a
POeticPotatoes <poeticpotatoes@gmail.com>
2025-08-29 15:48:13 +0800

    Add echo functionality

--- src/main/java/izayoi/Izayoi.java
+++ src/main/java/izayoi/Izayoi.java
@@ -1,13 +1,27 @@
 package izayoi;
 
+import java.util.Scanner;
+
 public class Izayoi {
+	public static final Scanner scanner = new Scanner(System.in);
 	
     public static void main(String[] args) {
     	logLine();
     	logString(" Hello! I'm Sakuya Izayoi\n What can I do for you?");
-    	logString(" Bye. Hope to see you again soon!");
+    	String command = getInput();
+    	while (!command.equals("bye")) {
+    		logString(command);
+    		command = getInput();
+    	}
+    	logString(" Hmph. About time you stopped talking... Do come again.");
     }
     
+    private static String getInput() {
+    	String s = scanner.nextLine();
+    	logLine();
+    	return s;
+    }
+
     private static void logString(String s) {
 		System.out.println(s);
 		logLine();
